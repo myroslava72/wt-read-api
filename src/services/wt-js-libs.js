6d1d79c54406ad7ca60d3422ebe189586eb81769
Jirka Chadima <chadima.jiri@gmail.com>
2019-05-24 10:44:14 +0200

    chore: Bumps @windingtree/wt-js-libs and adapts to new interface

--- src/services/wt-js-libs.js
+++ src/services/wt-js-libs.js
@@ -1,3 +1,4 @@
+const Web3Utils = require('web3-utils');
 const { AIRLINE_SEGMENT_ID, HOTEL_SEGMENT_ID } = require('../constants');
 
 const { config } = require('../config');
@@ -31,17 +32,23 @@ async function passesTrustworthinessTest (hotelAddress, guarantee) {
   }
   const trustClueClient = getInstance().getTrustClueClient();
   try {
-    // 1. decode guarantor from guarantee
-    const data = trustClueClient.verifyAndDecodeSignedData(guarantee.claim, guarantee.signature, 'guarantor');
-    if (hotelAddress !== data.hotel) {
-      throw new Error(`Guarantee seems to be for a different hotel at ${data.hotel}.`);
+    // 1. decode guarantor from guarantee and verify signature
+    const data = JSON.parse(Web3Utils.hexToUtf8(guarantee.claim));
+    trustClueClient.verifySignedData(guarantee.claim, guarantee.signature, (actualSigner) => {
+      if (hotelAddress !== data.hotel) {
+        throw new Error(`Guarantee seems to be for a different hotel at ${data.hotel}.`);
+      }
+      if (!data.expiresAt) {
+        throw new Error('Guarantee has no expiration.');
+      }
+      if ((new Date()).getTime() > data.expiresAt) {
+        throw new Error(`Guarantee expired at ${new Date(data.expiresAt)}.`);
+      }
+      if (Web3Utils.toChecksumAddress(data.guarantor) !== actualSigner) {
+        throw new Error(`Expected signer '${data.guarantor}' does not match the recovered one '${actualSigner}'`);
+      }
     }
-    if (!data.expiresAt) {
-      throw new Error('Guarantee has no expiration.');
-    }
-    if ((new Date()).getTime() > data.expiresAt) {
-      throw new Error(`Guarantee expired at ${new Date(data.expiresAt)}.`);
-    }
+    );
     // 2. check guarantors trust levels
     const trustworthinessTestResults = await trustClueClient.interpretAllValues(data.guarantor);
     // 3. return single boolean value - this expects all clues to have interpret function that returns boolean
--- test/services/wt-js-libs.spec.js
+++ test/services/wt-js-libs.spec.js
@@ -2,6 +2,7 @@
 /* eslint-disable no-unused-expressions */
 const { expect } = require('chai');
 const sinon = require('sinon');
+const Web3Utils = require('web3-utils');
 const { getTrustClueClient, passesTrustworthinessTest } = require('../../src/services/wt-js-libs');
 const { config } = require('../../src/config');
 
@@ -10,22 +11,20 @@ describe('WtJsLibs wrapper', () => {
     it('should call wtJsLibs', () => {
       const getClientSpy = sinon.spy(config.wtLibs, 'getTrustClueClient');
       const client = getTrustClueClient();
-      expect(client).to.have.property('verifyAndDecodeSignedData');
+      expect(client).to.have.property('verifySignedData');
       expect(getClientSpy.callCount).to.be.equal(1);
       getClientSpy.restore();
     });
   });
 
   describe('passesTrustworthinessTest', () => {
-    let origGetTrustClueClient, baseTrustClueClient, warnLogStub;
+    let origGetTrustClueClient, baseTrustClueClient, warnLogStub, guarantee;
+    const hotelAddress = '0xDbdF9B636dF72dD35fB22bb105d8e1bB9a0957C8';
 
     beforeEach(() => {
       origGetTrustClueClient = config.wtLibs.getTrustClueClient;
       baseTrustClueClient = {
-        verifyAndDecodeSignedData: () => ({
-          hotel: '123',
-          expiresAt: (new Date()).setDate((new Date()).getDate() + 1),
-        }),
+        verifySignedData: (claim, sig, verFn) => verFn('0x5808b3232de474e155a8d915cc588D5095C13631'),
         interpretAllValues: () => Promise.resolve([
           { name: 'test-clue', value: true },
           { name: 'test-clue-2', value: true },
@@ -33,6 +32,10 @@ describe('WtJsLibs wrapper', () => {
       };
       config.wtLibs.getTrustClueClient = () => (baseTrustClueClient);
       warnLogStub = sinon.stub(config.logger, 'warn');
+      guarantee = {
+        claim: '0x7b22686f74656c223a22307844626446394236333664463732644433356642323262623130356438653162423961303935374338222c2267756172616e746f72223a22307835383038623332333264653437346531353561386439313563633538384435303935433133363331222c22657870697265734174223a313536313336343436393030307d',
+        signature: '0x6bd1846b460d5ce14a329240db33f83ecbcce795e0ae701ebdb3060eea686252525ddde8e7769e748b03df5acecf104acd605726c469d23470223de03243a9571c',
+      };
     });
 
     afterEach(() => {
@@ -42,12 +45,12 @@ describe('WtJsLibs wrapper', () => {
 
     it('should always return true if no guarantee is required', async () => {
       config.checkTrustClues = false;
-      expect(await passesTrustworthinessTest('123', { claim: '123', signature: '123' })).to.be.equal(true);
-      expect(await passesTrustworthinessTest('123', 'rubbish')).to.be.equal(true);
-      expect(await passesTrustworthinessTest('123', {
+      expect(await passesTrustworthinessTest(hotelAddress, guarantee)).to.be.equal(true);
+      expect(await passesTrustworthinessTest(hotelAddress, 'rubbish')).to.be.equal(true);
+      expect(await passesTrustworthinessTest(hotelAddress, {
         claim: 'rubbish',
       })).to.be.equal(true);
-      expect(await passesTrustworthinessTest('123', {
+      expect(await passesTrustworthinessTest(hotelAddress, {
         claim: 'rubbish',
         signature: 'rubbish',
       })).to.be.equal(true);
@@ -55,14 +58,16 @@ describe('WtJsLibs wrapper', () => {
     });
 
     it('should return true if at all trust clues interpret as true', async () => {
-      expect(await passesTrustworthinessTest('123', { claim: '123', signature: '123' })).to.be.equal(true);
+      expect(await passesTrustworthinessTest(hotelAddress, guarantee)).to.be.equal(true);
     });
 
     it('should return false if no or malformed guarantee is provided but should be checked', async () => {
       expect(await passesTrustworthinessTest('123', undefined)).to.be.equal(false);
       expect(await passesTrustworthinessTest('123', {})).to.be.equal(false);
-      expect(await passesTrustworthinessTest('123', { claim: '123' })).to.be.equal(false);
-      expect(await passesTrustworthinessTest('123', { signature: '123' })).to.be.equal(false);
+      expect(await passesTrustworthinessTest('123', { claim: guarantee.claim })).to.be.equal(false);
+      expect(await passesTrustworthinessTest('123', { signature: guarantee.signature })).to.be.equal(false);
+      expect(await passesTrustworthinessTest('123', { claim: 'rubbish' })).to.be.equal(false);
+      expect(await passesTrustworthinessTest('123', { signature: 'rubbish' })).to.be.equal(false);
     });
 
     it('should return false and log if at least one trust clue interprets as false', async () => {
@@ -70,52 +75,63 @@ describe('WtJsLibs wrapper', () => {
         { name: 'test-clue', value: true },
         { name: 'test-clue-2', value: false },
       ]);
-      expect(await passesTrustworthinessTest('123', { claim: '123', signature: '123' })).to.be.equal(false);
+      expect(await passesTrustworthinessTest(hotelAddress, guarantee)).to.be.equal(false);
     });
 
     it('should return false and log if signature is not verified', async () => {
-      baseTrustClueClient.verifyAndDecodeSignedData = () => {
+      baseTrustClueClient.verifySignedData = () => {
         throw new Error('Signature not verified.');
       };
-      expect(await passesTrustworthinessTest('123', { claim: '123', signature: '123' })).to.be.equal(false);
+      expect(await passesTrustworthinessTest(hotelAddress, guarantee)).to.be.equal(false);
       expect(warnLogStub.callCount).to.be.eql(1);
       expect(warnLogStub.firstCall.args[0]).to.match(/signature not verified/i);
     });
 
+    it('should return false and log if signer does not match', async () => {
+      baseTrustClueClient.verifySignedData = (claim, sig, verFn) => verFn('1234');
+      expect(await passesTrustworthinessTest(hotelAddress, guarantee)).to.be.equal(false);
+      expect(warnLogStub.callCount).to.be.eql(1);
+      expect(warnLogStub.firstCall.args[0]).to.match(/does not match/i);
+    });
+
     it('should return false and log if guarantee has no hotel information', async () => {
-      baseTrustClueClient.verifyAndDecodeSignedData = () => ({
-        expiresAt: (new Date()).setDate((new Date()).getDate() - 1),
-      });
-      expect(await passesTrustworthinessTest('123', { claim: '123', signature: '123' })).to.be.equal(false);
+      guarantee.claim = Web3Utils.utf8ToHex(JSON.stringify({
+        guarantor: '0x5808b3232de474e155a8d915cc588D5095C13631',
+        expiresAt: 1561364469000,
+      }));
+      expect(await passesTrustworthinessTest(hotelAddress, guarantee)).to.be.equal(false);
       expect(warnLogStub.callCount).to.be.eql(1);
       expect(warnLogStub.firstCall.args[0]).to.match(/for a different hotel/i);
     });
 
     it('should return false and log if hotelAddress does not match the guarantee', async () => {
-      baseTrustClueClient.verifyAndDecodeSignedData = () => ({
+      guarantee.claim = Web3Utils.utf8ToHex(JSON.stringify({
         hotel: '456',
-        expiresAt: (new Date()).setDate((new Date()).getDate() + 1),
-      });
-      expect(await passesTrustworthinessTest('123', { claim: '123', signature: '123' })).to.be.equal(false);
+        guarantor: '0x5808b3232de474e155a8d915cc588D5095C13631',
+        expiresAt: 1561364469000,
+      }));
+      expect(await passesTrustworthinessTest(hotelAddress, guarantee)).to.be.equal(false);
       expect(warnLogStub.callCount).to.be.eql(1);
       expect(warnLogStub.firstCall.args[0]).to.match(/for a different hotel/i);
     });
 
     it('should return false and log if guarantee is expired', async () => {
-      baseTrustClueClient.verifyAndDecodeSignedData = () => ({
-        hotel: '123',
+      guarantee.claim = Web3Utils.utf8ToHex(JSON.stringify({
+        hotel: '0xDbdF9B636dF72dD35fB22bb105d8e1bB9a0957C8',
+        guarantor: '0x5808b3232de474e155a8d915cc588D5095C13631',
         expiresAt: (new Date()).setDate((new Date()).getDate() - 1),
-      });
-      expect(await passesTrustworthinessTest('123', { claim: '123', signature: '123' })).to.be.equal(false);
+      }));
+      expect(await passesTrustworthinessTest(hotelAddress, guarantee)).to.be.equal(false);
       expect(warnLogStub.callCount).to.be.eql(1);
       expect(warnLogStub.firstCall.args[0]).to.match(/guarantee expired/i);
     });
 
     it('should return false and log if guarantee has no expiration information', async () => {
-      baseTrustClueClient.verifyAndDecodeSignedData = () => ({
-        hotel: '123',
-      });
-      expect(await passesTrustworthinessTest('123', { claim: '123', signature: '123' })).to.be.equal(false);
+      guarantee.claim = Web3Utils.utf8ToHex(JSON.stringify({
+        hotel: '0xDbdF9B636dF72dD35fB22bb105d8e1bB9a0957C8',
+        guarantor: '0x5808b3232de474e155a8d915cc588D5095C13631',
+      }));
+      expect(await passesTrustworthinessTest(hotelAddress, guarantee)).to.be.equal(false);
       expect(warnLogStub.callCount).to.be.eql(1);
       expect(warnLogStub.firstCall.args[0]).to.match(/guarantee has no expiration/i);
     });
